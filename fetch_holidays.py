d1c6c4be244be4e4eb74a3b791fac59b7953128e
Lukas Mertens <git@lukas-mertens.de>
2023-05-05 11:39:54 +0200

    feat: delete directory first

--- fetch_holidays.py
+++ fetch_holidays.py
@@ -7,13 +7,21 @@ current_year = datetime.now().year
 end_year = current_year + 10
 base_url = "https://feiertage-api.de/api/"
 
-os.makedirs("holidays", exist_ok=True)
+# Remove existing JSON files in the holidays directory
+holidays_dir = "holidays"
+if os.path.exists(holidays_dir):
+    for file in os.listdir(holidays_dir):
+        if file.endswith(".json"):
+            os.remove(os.path.join(holidays_dir, file))
+
+os.makedirs(holidays_dir, exist_ok=True)
 
 for year in range(2010, end_year + 1):
-  response = requests.get(f"{base_url}?jahr={year}")
-  if response.status_code == 200:
-      holidays = response.json()
-      with open(f"holidays/holidays_{year}.json", "w") as f:
-          json.dump(holidays, f, ensure_ascii=False, indent=4)
-  else:
-      print(f"Error fetching holidays for the year {year}")
+    response = requests.get(f"{base_url}?jahr={year}")
+    if response.status_code == 200:
+        holidays = response.json()
+        with open(f"holidays/holidays_{year}.json", "w") as f:
+            json.dump(holidays, f, ensure_ascii=False, indent=4)
+    else:
+        print(f"Error fetching holidays for the year {year}")
+
